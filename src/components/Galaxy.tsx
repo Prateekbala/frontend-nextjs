1dae43b53f0dbe4bd3cc7456a57ce91c4411abdf
Prateekbala <145365208+Prateekbala@users.noreply.github.com>
2024-06-14 16:26:49 +0530

    Update Galaxy.tsx

--- src/components/Galaxy.tsx
+++ src/components/Galaxy.tsx
@@ -23,7 +23,7 @@ function FeaturedGalaxy() {
         <div>
             <div className="text-center">
                 <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">FEATURED Galaxy</h2>
-                <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Explore New Horizon</p>
+                <p className="mt-4 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">Explore New Horizon</p>
             </div>
         </div>
         <div className="h-[45rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
